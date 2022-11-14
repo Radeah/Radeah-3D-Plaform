23675aa60703ec3e84e85f18bdd284fcdd219670
Radeah <113103602+Radeah@users.noreply.github.com>
2022-11-14 11:50:46 +0000

    fix stuff

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     float maxSpeed = 0.01f;
-    float rotation = 0.0f;
-    float camRotation = 0.0f;
+    float rotation = 0.2f;
+    float camRotation = 0.2f;
 
     GameObject cam;
+
+    float rotationSpeed = 2.0F;
+    float camRotationSpeed = 1.5f;
 
     void Start()
     {
@@ -20,8 +23,11 @@ public class CharacterController : MonoBehaviour
     {
         transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed);
 
-        rotation = rotation + Input.GetAxis("Mouse X");
+        rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(1.0f, rotation, 0.0f));
+
+        camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+        cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
 
 
     }
