93cc50c375dfafbddaeed3cc60e4e145b4e5ab5b
Radeah <113103602+Radeah@users.noreply.github.com>
2022-11-15 12:40:55 +0000

    finish off walls

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -17,6 +17,9 @@ public class CharacterController : MonoBehaviour
     float rotationSpeed = 2.0F;
     float camRotationSpeed = 1.5f;
 
+    float jumpForce = 400f;
+    
+
     void Start()
     {
         cam = GameObject.Find("Main Camera");
@@ -26,13 +29,19 @@ public class CharacterController : MonoBehaviour
     {
         isOnGround = Physics.CheckSphere(GroundChecker.transform.position, 0.1f, groundLayer);
 
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+
         Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
         myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
-        transform.rotation = Quaternion.Euler(new Vector3(1.0f, rotation, 0.0f));
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
 
         camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+        
         cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
 
 
---
+++ Assets/Pickup.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
