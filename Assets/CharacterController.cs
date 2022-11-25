faeab09c9b61733ee97fddeb4e1ffd70b4915842
Radeah <113103602+Radeah@users.noreply.github.com>
2022-11-25 15:16:45 +0000

    Revert "Fix coding stuff"
    This reverts commit 441b40be4ff1fa61238177040d989cab5dcb1432, reversing
    changes made to f1fb8c02984e2ef2e4cb5bcf6a471dc6405964e1.

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    float maxSpeed = 0.01f;
+    float rotation = 0.2f;
+    float camRotation = 0.2f;
+    GameObject cam;
+    Rigidbody myRigidbody;
+    
+    bool isOnGround;
+    public Gameobject GroundChecker;
+    public LayerMask groundLayer;
+
+    float rotationSpeed = 2.0F;
+    float camRotationSpeed = 1.5f;
+
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+        void Update()
+{
+        isOnGround = Physics.CheckerSphere(GroundChecker.transform.position, 0.1f, grounLayer);
+
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
+        
+        transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed);
+
+        rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
+        transform.rotation = Quaternion.Euler(new Vector3(1.0f, rotation, 0.0f));
+
+        camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+        cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
+
+
+    
+}
